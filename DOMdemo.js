89753cc105bc3b3a0d8dbffb7e34e25d52c4de83
ved1150 <vppatel1771@gmail.com>
2022-07-15 16:09:59 +0530

    QUERYSELECTORALL

--- DOMdemo.js
+++ DOMdemo.js
@@ -34,15 +34,40 @@ let addItems= document.getElementById("main");
 // }
 
 // GETELEMENTSBYTAGNAME
-let li=document.getElementsByTagName("li");
+// let li=document.getElementsByTagName("li");
 
-li[2].style.background = "green";
-// item[0].style.fontWeight ="900";
-// item.style.color ="yellow";
+// li[2].style.background = "green";
+// // item[0].style.fontWeight ="900";
+// // item.style.color ="yellow";
 
-for(let i=0;i<li.length;i++){
-    li[i].style.backgroundColor ="yellow";
-    li[i].style.fontWeight ="900";
+// for(let i=0;i<li.length;i++){
+//     li[i].style.backgroundColor ="yellow";
+//     li[i].style.fontWeight ="900";
 
-}
+// }
 
+// QUERYSELECTOR
+
+// let secItem = document.querySelector(".list-group-item:nth-child(2)");
+// secItem.style.backgroundColor="green";
+// let hide = document.querySelector(".list-group-item:nth-child(3)");
+// hide.textContent=" "
+
+// QUERYSELECTORALL
+
+//  let secItem = document.querySelectorAll(".list-group-item");
+//  secItem[1].style.color = "green"
+
+//  let odd = document.querySelectorAll("li:nth-child(odd)")
+
+//  for(let i =0;i<odd.length;i++){
+// odd[i].style.backgroundColor = "gray"
+//  }
+
+//  let even = document.querySelectorAll("li:nth-child(even)")
+
+//  for(let i =0;i<even.length;i++){
+// even[i].style.backgroundColor = "lightgray"
+//  }
+
+//  diff b/w queryselector and queryselectorall -->queryselector only select one item in the html and queryselectorall grab many item in the server and also specifi even and odd 
